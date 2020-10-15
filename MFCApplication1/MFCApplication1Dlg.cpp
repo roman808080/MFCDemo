34766887707c0a9629a5af189db2b96499be1f94
Roman Herasymenko <Roman_Herasymenko@epam.com>
2020-10-15 19:04:34 +0200

    Init commit.

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,222 @@
+
+// MFCApplication1Dlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+namespace
+{
+	const int kFirstToolbarButton = 0;
+}
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg dialog
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg message handlers
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	if (!m_wndToolBar.Create(this, WS_CHILD | WS_VISIBLE | CBRS_TOP) ||
+		!m_wndToolBar.LoadToolBar(IDR_TOOLBAR1))
+	{
+		TRACE0("Failed to Create Dialog Toolbar\n");
+		EndDialog(IDCANCEL);
+	}
+
+	m_wndToolBar.ModifyStyle(NULL, TBSTYLE_LIST | TBBS_AUTOSIZE);
+
+	UINT nSecondButtonID;
+	UINT nSecondButtonStyle;
+	int iSecondButtonImage;
+
+	m_wndToolBar.GetButtonInfo(kFirstToolbarButton, nSecondButtonID, nSecondButtonStyle, iSecondButtonImage);
+	m_wndToolBar.SetButtonInfo(kFirstToolbarButton, nSecondButtonID, TBBS_CHECKBOX | TBBS_AUTOSIZE | BTNS_SHOWTEXT, iSecondButtonImage);
+
+	m_wndToolBar.SetButtonText(kFirstToolbarButton, TEXT("Mark Takeoff Complete"));
+
+	butD = TRUE;
+	CRect rcClientOld; // Old Client Rect
+	CRect rcClientNew; // New Client Rect with Tollbar Added
+
+	// Retrive the Old Client WindowSize
+	// Called to reposition and resize control bars in the client area of a window
+	// The reposQuery FLAG does not really traw the Toolbar. It only does the calculations.
+	// And puts the new ClientRect values in rcClientNew so we can do the rest of the Math.
+
+	GetClientRect(rcClientOld);
+	RepositionBars(AFX_IDW_CONTROLBAR_FIRST, AFX_IDW_CONTROLBAR_LAST, 0, reposQuery, rcClientNew);
+	// All of the Child Windows (Controls) now need to be moved so the Tollbar does not cover them up.
+	// Offest to move all child controls after adding Tollbar 
+	CPoint ptOffset(rcClientNew.left - rcClientOld.left, rcClientNew.top - rcClientOld.top);
+
+	CRect rcChild;
+	CWnd* pwndChild = GetWindow(GW_CHILD); //Handle to the Dialog Controls
+
+	while (pwndChild) // Cycle through all child controls 
+	{
+		pwndChild->GetWindowRect(rcChild); // Get the child control RECT
+		ScreenToClient(rcChild);
+
+		// Changes the Child Rect by the values of the claculated offset
+		rcChild.OffsetRect(ptOffset);
+		pwndChild->MoveWindow(rcChild, FALSE); // Move the Child Control
+		pwndChild = pwndChild->GetNextWindow();
+	}
+
+	CRect rcWindow;
+	// Get the RECT of the Dialog
+	GetWindowRect(rcWindow);
+
+	// Increase width to new Client Width
+	rcWindow.right += rcClientOld.Width() - rcClientNew.Width();
+
+	// Increase height to new Client Height
+	rcWindow.bottom += rcClientOld.Height() - rcClientNew.Height();
+	// Redraw Window
+	MoveWindow(rcWindow, FALSE);
+
+	// Now we REALLY Redraw the Toolbar
+	RepositionBars(AFX_IDW_CONTROLBAR_FIRST, AFX_IDW_CONTROLBAR_LAST, 0);
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CMFCApplication1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ MFCApplication1/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFCApplication1.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDR_TOOLBAR1                    129
+#define ID_BUTTON32774                  32774
+#define ID_BUTTON32780                  32780
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
